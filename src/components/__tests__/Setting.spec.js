b5aa03f5aa2c71b6c9118e2ea5a0e12d31eed1bc
知念英弥 <s19013@std.it-college.ac.jp>
2023-10-30 21:28:09 +0900

    テストしたいのを予めメモしておいた

---
+++ src/components/__tests__/Setting.spec.js
@@ -0,0 +1,14 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import Setting from '@/components/Setting.vue'
+
+// describe('Setting', () => {
+//   it('', () => {
+//     expect().toContain()
+//   })
+// })
+
+// テストしたいこと
+// プルダウンのデフォルト
+// 変更が反映されているか
