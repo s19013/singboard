c0593ab6de969d2233a34d0c8b6d4e85fce42527
知念英弥 <s19013@std.it-college.ac.jp>
2023-11-04 20:47:18 +0900

    とりあえず書いてみた

--- e2e/vue.spec.js
+++ e2e/vue.spec.js
@@ -1,8 +1,36 @@
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
-// See here how to get started:
-// https://playwright.dev/docs/intro
-test('visits the app root url', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
-})
+test('test', async ({ page }) => {
+  await page.goto('http://127.0.0.1:5173/singboard');
+  await page.locator('textarea[name="body"]').click();
+  await page.locator('textarea[name="body"]').fill('playwrightTest');
+  await page.locator('div').filter({ hasText: /^文字の大きさ特大大中小極小$/ }).getByRole('combobox').selectOption('larger');
+
+  // 再現されていないみたい｡
+  await page.locator('div').filter({ hasText: /^文字の色HEXRGBA$/ }).locator('canvas').first().click({
+    button: 'left',
+    position: {
+      x: 147,
+      y: 74
+    }
+  });
+  await page.locator('div').filter({ hasText: /^背景の色HEXRGBA$/ }).locator('canvas').first().click({
+    button: 'left',
+    position: {
+      x: 65,
+      y: 9
+    }
+  });
+
+  // 実行前
+  await page.screenshot({ path: 'before_execuing.jpg', fullPage: false });
+
+  // 実行中
+  await page.getByRole('button', { name: '表示' }).click();
+  await page.screenshot({ path: 'execuing.jpg', fullPage: false });
+  await expect(page.getByText('playwrightTest')).toBeVisible();
+
+  // 実行後
+  await page.getByRole('button', { name: '✕ 停止' }).click();
+  await page.screenshot({ path: 'after_execution.jpg', fullPage: false });
+});
